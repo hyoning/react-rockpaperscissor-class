80d8f2e6290e96f86f6a1e71021d4211a2f0606d
김효은 <hyoeun@gmail.com>
2024-03-20 13:27:21 +0900

    클래스 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import {useState} from 'react'
-import './App.css';
-import Box from './component/Box.js'
-// 1. 박스 두개 (타이틀, 사진, 결과)
-// 2. 가위 바위 보 버튼이 있다. 
-// 3. 버튼을 클릭하면 클릭한 값이 박스에 보임
-// 4. 컴퓨터는 랜덤하게 아이템 선택이 된다. 
-// 5. 3~4의 결과를 가지고 누가 이겼는지 승패를 따진다.
-// 6. 승패 결과에 따라 테두리 색이 바뀐다. (이기면- 초록, 지면-빨강, 비기면-검정)
+import React, { Component } from "react";
+import "./App.css";
+import Box from "./component/Box";
+
 const choice = {
   rock : {
     name: "Rock",
@@ -20,18 +15,25 @@ const choice = {
     name: "Paper",
     img: "img/paper.png"
   }
-}
-function App() {
-  const [userSelect, setUserSelect] = useState(choice.scissors)
-  const [computerSelect, setComputerSelect] = useState(choice.scissors)
-  const [result, setResult] = useState("")
-  const play = (userChoice) =>{
-    setUserSelect(choice[userChoice])
-    let computerChoice = randomChoice()
-    setComputerSelect(computerChoice)
-    setResult(judgement(choice[userChoice], computerChoice))
+};
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      userSelect : choice.scissors,
+      computerSelect : choice.scissors,
+      result : "",
+    }
+  }
+  play = (userChoice) => {
+    let computerChoice = this.randomChoice();
+    this.setState({
+      userSelect : choice[userChoice],
+      computerSelect : computerChoice,
+      result : this.judgement(choice[userChoice], computerChoice)
+    });
   } 
-  const judgement = (user, computer) => {
+  judgement = (user, computer) => {
     if(user.name === computer.name){
       return "Tie"
     } else if(user.name === "Rock"){
@@ -42,26 +44,27 @@ function App() {
       return computer.name === "Rock" ? "Win" : "Lose";
     }
   }
-  const randomChoice = () => {
+  randomChoice = () => {
     let itemArray = Object.keys(choice); // 객체에 키값만 뽑아서 배열로 만들어주는 Object.keys
     let randomItem = Math.floor(Math.random()*itemArray.length);
-    let final = itemArray[randomItem]
-    return choice[final]
+    let final = itemArray[randomItem];
+    return choice[final];
   }
-  return (
-    <div className="wrap">
-      <div className="title">가위 바위 보 게임!<p>아래 버튼을 눌러 가위,바위,보 게임을 해봐!</p></div>
-      <div className='buttonWrap'>
-        <button className="button-scissors" onClick={() => play("scissors")}>가위</button>
-        <button className="button-rock" onClick={() => play("rock")}>바위</button>
-        <button className="button-paper"  onClick={() => play("paper")}>보</button>
+  render(){
+    return (
+      <div className="wrap">
+        <div className="title">가위 바위 보 게임!<p>아래 버튼을 눌러 가위,바위,보 게임을 해봐!</p></div>
+        <div className='buttonWrap'>
+          <button className="button-scissors" onClick={() => this.play("scissors")}>가위</button>
+          <button className="button-rock" onClick={() => this.play("rock")}>바위</button>
+          <button className="button-paper"  onClick={() => this.play("paper")}>보</button>
+        </div>
+        <div className="main">
+          <Box title="You" item={this.state.userSelect} result={this.state.result} />
+          <Box title="Computer" item={this.state.userSelect} result={this.state.result} />
+        </div>
       </div>
-      <div className="main">
-        <Box title="You" item={userSelect} result={result}/>
-        <Box title="Computer" item={computerSelect} result={result}/>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default App;
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,24 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Box = (props) => {
-  let result;
-  if (
-    props.title === "Computer" &&
-    props.result !== "Tie" &&
-    props.result !== ""
-  ) { 
-    result = props.result === "Win" ? "Lose" : "Win";
-  } else {
-    result = props.result;
+export default class Box extends Component {
+  constructor(){
+    super();
+    this.result = "";
   }
-  return (
-    <div className={`box ${result}`}>
-      <h1>{props.title}</h1>
-      <div className="item-name">{props.item && props.item.name}</div>
-      <img className="item-img" src={props.item && props.item.img} alt=""/>
-      <div className={`item-result item-${result}`}>{result}</div>
-    </div>
-  );
+  getResult = () =>{
+    if (
+      this.props.title === "Computer" &&
+      this.props.result !== "Tie" &&
+      this.props.result !== ""
+    ) { 
+      this.result = this.props.result === "Win" ? "Lose" : "Win";
+    } else {
+      this.result = this.props.result;
+    }
+  }
+  render(){
+    this.getResult();
+    return (
+      <div className={`box ${this.result}`}>
+        <h1>{this.props.title}</h1>
+        <div className="item-name">{this.props.item && this.props.item.name}</div>
+        <img className="item-img" src={this.props.item && this.props.item.img} alt=""/>
+        <div className={`item-result item-${this.result}`}>{this.result}</div>
+      </div>
+    );
+  }
 };
-
-export default Box;
